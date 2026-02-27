fc7eedd6c38b90b01393648ac973df912b12c1d0
Harshal Vaidya <hvvaidya@usc.edu>
2026-02-26 19:10:25 -0800

    fixed imports in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,10 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import pg from 'pg';
+import { Pool } from '@neondatabase/serverless';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
 }
 
-export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool);
-
