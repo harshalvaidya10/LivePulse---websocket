40c61755fbc3bbd07da87f8e9b17d4c06f62cf1f
Harshal Vaidya <hvvaidya@usc.edu>
2026-02-27 22:27:24 -0800

    issues fixed which were suggested by code rabbit

--- arcjet.js
+++ arcjet.js
@@ -1,17 +1,18 @@
 import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
-import { SQLiteDeleteBase } from "drizzle-orm/sqlite-core";
 
 const arcjetKey = process.env.ARCJET_KEY;
-const arcjetMode = process.env.ARCJECT_MODE === 'DRY_RUN' ? 'DRY_RUN' : 'LIVE';
+const arcjetMode = process.env.ARCJET_MODE === 'DRY_RUN' ? 'DRY_RUN' : 'LIVE';
 
-if(!arcjetKey) throw new Error('ARCJET_KEY environment variable is missing');
+if (!arcjetKey) {
+    console.warn('ARCJET_KEY is missing. Arcjet protection is disabled.');
+}
 
 export const httpArcjet = arcjetKey ? 
     arcjet({    
         key: arcjetKey,
         rules: [
             shield({mode: arcjetMode}),
-            detectBot({mode: arcjetMode, allow: ['CATEGORY: SEARCH_ENGINE', 'CATEGORY:PREVIEW' ]}),
+            detectBot({mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW' ]}),
             slidingWindow({ mode: arcjetMode, interval: '10s', max: 50})
         ]
     }):null;
@@ -21,7 +22,7 @@ export const wsArcjet = arcjetKey ?
         key: arcjetKey,
         rules: [
             shield({ mode: arcjetMode }),
-            detectBot({ mode: arcjetMode, allow: ['CATEGORY: SEARCH_ENGINE', 'CATEGORY:PREVIEW'] }),
+            detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'] }),
             slidingWindow({ mode: arcjetMode, interval: '2s', max: 5 })
         ]
     }):null;
